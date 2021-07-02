6f2118e19b45abc15f4d61199be100d23cc58cb9
Diana Patron <patrondiana1309@gmail.com>
2021-07-02 18:55:57 -0500

    Add Meal Class implementation

--- src/snake/model/Meal.java
+++ src/snake/model/Meal.java
@@ -1,4 +1,23 @@
 package snake.model;
 
-public class Meal {
+public class Meal extends GameObject{
+    private int value;
+
+    public Meal(int x, int y, int value) {
+        super(x, y);
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public void update() {
+
+    }
 }
