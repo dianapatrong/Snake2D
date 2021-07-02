a8cc9ee11f42f674338fd7ba249c281556d18cf7
Diana Patron <patrondiana1309@gmail.com>
2021-07-02 18:34:53 -0500

    Add project structure and GameObjec class

---
+++ src/snake/model/GameObject.java
@@ -0,0 +1,53 @@
+package snake.model;
+
+public abstract class GameObject {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public GameObject(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public abstract void update();
+
+    public boolean isColliding(GameObject another){
+        return this.x == another.x && this.y == another.y;
+    }
+}
